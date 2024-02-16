638ee9788b0f5686534f2f0d3436755ba6843880
keshav-itobuz <keshav@itobuz.com>
2024-02-16 17:16:29 +0530

    final update

--- bgButtonCreator/index.js
+++ bgButtonCreator/index.js
@@ -1,10 +1,10 @@
 let count = 0;
-let buttonSection = document.getElementById("buttonSection");
+const buttonSection = document.getElementById("buttonSection");
 
 function getRandomColor() {
-    var letters = '0123456789ABCDEF';
-    var color = '#';
-    for (var i = 0; i < 6; i++) {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
         color += letters[Math.floor(Math.random() * 16)];
     }
     return color;
@@ -15,7 +15,7 @@ document.body.addEventListener('click', (e) => {
         document.body.style.backgroundColor = getRandomColor();
         count++;
         if (count % 4 === 0) {
-            let btn = document.createElement('button');
+            const btn = document.createElement('button');
             btn.innerText = "Change"
             btn.setAttribute("class", "p-4 rounded-2");
             buttonSection.appendChild(btn);
